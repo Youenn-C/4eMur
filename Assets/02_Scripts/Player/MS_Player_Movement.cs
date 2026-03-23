fafd78e65cfd71e33e69dd075766025cf0b4c6f1
ESMA-YouennC <y.chapelliere@rns.ecolescreatives.com>
2026-03-23 14:40:56 +0100

    Feature - Player : Animations
    - Added basic Idle and Walk animation

--- Assets/02_Scripts/Player/MS_Player_Movement.cs
+++ Assets/02_Scripts/Player/MS_Player_Movement.cs
@@ -4,14 +4,15 @@ using Rewired;
 public class MS_Player_Movement : MonoBehaviour
 {
     [Header("Variables PlayerMovement"), Space(5)]
-    public float playerAtraction;
-    [SerializeField] private float _forwardMovement;
-    [SerializeField] private float _lateralMovement;
+    private float _forwardMovement;
+    private float _lateralMovement;
     [Space(5)]
     public bool isMoving;
     
     void Start()
     {
+        MS_Player_Controller.Instance.playerCameraAnimator.SetTrigger("Idle");
+        
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -24,6 +25,12 @@ public class MS_Player_Movement : MonoBehaviour
         // Mises à jour des directions basées sur les boutons
         _forwardMovement = MS_Player_Controller.Instance.player.GetAxis("Forward_Movement");
         _lateralMovement = MS_Player_Controller.Instance.player.GetAxis("Lateral_Movement");
+        
+        if (_forwardMovement == 0 && _lateralMovement == 0) isMoving = false;
+        else isMoving = true;
+        
+        if (isMoving) MS_Player_Controller.Instance.playerCameraAnimator.SetTrigger("Walk");
+        else MS_Player_Controller.Instance.playerCameraAnimator.SetTrigger("Idle");
     }
     
     void FixedUpdate()
