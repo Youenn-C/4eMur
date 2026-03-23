210818594dea7779634d83ec70fa835d36aaf5cb
ESMA-YouennC <y.chapelliere@rns.ecolescreatives.com>
2026-03-23 10:04:01 +0100

    Feature - 3C : Add Movement and rotation of camera
    - Added Player_Movement system :
      - basic movement
    
    - Added Player_Camera_Movment system :
      - basic modular camera rotation
    
    TODO :
    - [ OPTIONAL ] Movement animations

---
+++ Assets/02_Scripts/Player/MS_Player_Controller.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Rewired;
+
+public class MS_Player_Controller : MonoBehaviour
+{
+    public static MS_Player_Controller Instance;
+    
+    [Header("References"), Space(5)]
+    public Rigidbody playerRigidbody;
+    public GameObject playerGameObject;
+    public GameObject cinemachineTargetGameObject;
+    
+    [Header("Player Scripts"), Space(5)]
+    public MS_Player_Movement playerMovement;
+    public MS_Player_Camera_Movement playerCameraMovement;
+    
+    [Header("Variables"), Space(5)]
+    public float playerSpeed;
+    [Space(5)]
+    public bool playerCanMove = true;
+    public bool playerCanLookAround = true;
+    
+    [Header("Rewired"), Space(5)]
+    public int playerID;
+    public Player player;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(this);
+        
+        player = ReInput.players.GetPlayer(playerID);
+    }
+}
