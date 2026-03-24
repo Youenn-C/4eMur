358ddea3f5e2e95a714846f5332c3ea5160ff0d6
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2026-03-24 15:59:50 +0100

    feature - MG4 :
    Added :
    - Texture for feedback
    
    TO DO :
    - fix randomize position for button and add condition for it

--- Assets/02_Scripts/MiniGames/MJ4_Playlist_A_Vider/Datas/SO_Music_Datas.cs
+++ Assets/02_Scripts/MiniGames/MJ4_Playlist_A_Vider/Datas/SO_Music_Datas.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "SO_MusicDatas", menuName = "Scriptable Objects/SO_MusicDatas")]
 public class SO_MusicDatas : ScriptableObject
 {
-    
+    public string musicName;
+    public Sprite musicIcon;
 }
--- Assets/02_Scripts/MiniGames/MJ4_Playlist_A_Vider/MS_Music_Item.cs
+++ Assets/02_Scripts/MiniGames/MJ4_Playlist_A_Vider/MS_Music_Item.cs
@@ -1,19 +1,54 @@
-using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
-public class MS_Music_Item : MonoBehaviour
+public class MS_Music_Item : MonoBehaviour, IPointerEnterHandler
 {
+    [Header("References"), Space(5)]
+    [SerializeField] private TMP_Text _musicName;
+    [SerializeField] private Image _musicIcone;
+    [SerializeField] private Collider2D _buttonCollider2D;
+    [SerializeField] private Button _button;
     [SerializeField] private SO_MusicDatas _currentMusicDatas;
+    
+    [Header("Variables"), Space(5)]
+    [SerializeField] private Vector2 _topLeftMaxPosition;
+    [SerializeField] private Vector2 _bottomRightMaxPosition;
+    [SerializeField] private Vector2 _randomizedPosition;
+    
+    
 
-
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        float percent = Random.Range(0, 10);
+        
+        Debug.Log("Trigger");
+        
+        if (percent <= 0.5f)
+        {
+            Debug.Log("Randomize position");
+            RandomizePosition();
+        }
+    }
+    
+    void RandomizePosition()
+    {
+        _randomizedPosition.x = Random.Range(_topLeftMaxPosition.x, _bottomRightMaxPosition.x);
+        _randomizedPosition.y = Random.Range(_topLeftMaxPosition.y, _bottomRightMaxPosition.y);
+        
+        _button.transform.position = _randomizedPosition;
+    }
+    
     private void Start()
     {
-        
+        _musicName.text = _currentMusicDatas.musicName;
+        _musicIcone.sprite = _currentMusicDatas.musicIcon;
     }
 
     public void RemoveMusicFromQueue()
     {
         Destroy(gameObject);
     }
-    
 }
