6e9a4000740025f817d1c3cdba7e477b90b1109b
BlondeauLouis <l.blondeau@rns.ecolescreatives.com>
2026-03-24 10:36:28 +0100

    Début recréation MG prise Jack

---
+++ Assets/02_Scripts/MiniGames/MiniGamesManager.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class MiniGamesManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _jackCanvas;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _jackCanvas.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        _jackCanvas.SetActive(true);
+    }
+}
