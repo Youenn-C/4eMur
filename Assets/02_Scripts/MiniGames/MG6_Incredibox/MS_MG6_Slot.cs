6bf292d74e8524ec347a684de1010ead721f741e
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2026-03-25 19:42:45 +0100

    Feature MG6_Incredibox : Setup
    Added :
    - Basic UI
    - Minigame Manager script
    - Objective Prefab
    - Slot Prefab & Script

---
+++ Assets/02_Scripts/MiniGames/MG6_Incredibox/MS_MG6_Slot.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MS_MG6_Slot : MonoBehaviour
+{
+    [Header("Reference"), Space(5)]
+    [SerializeField] private Collider2D _collider2D;
+    
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
