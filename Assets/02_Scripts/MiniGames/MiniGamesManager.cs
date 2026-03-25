1fd2505dc898e6be294f0efa6ec74d576c6ae763
BlondeauLouis <l.blondeau@rns.ecolescreatives.com>
2026-03-25 10:32:10 +0100

    Tape Taupe opérationnel

--- Assets/02_Scripts/MiniGames/MiniGamesManager.cs
+++ Assets/02_Scripts/MiniGames/MiniGamesManager.cs
@@ -1,18 +1,32 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MiniGamesManager : MonoBehaviour
 {
     [SerializeField] private GameObject _jackCanvas, _tapeTaupeCanvas;
+    private List<GameObject> _minigames;
+    private GameObject _currentMinigame;
     private MS_Player_Controller _playerController;
+
+    [SerializeField] private TapeTaupe _tapeTaupeManager;
 
     private int _numberOfWins, _amountOfWinsToEarn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _minigames = new List<GameObject>();
+        _minigames.Add(_jackCanvas);
+        _minigames.Add(_tapeTaupeCanvas);
+        
         _playerController = MS_Player_Controller.Instance;
-        _jackCanvas.SetActive(false);
+        
+        foreach (GameObject minigame in _minigames)
+        {
+            minigame.SetActive(false);
+        }
         
         _numberOfWins = 0;
         _amountOfWinsToEarn = 3;
@@ -24,18 +38,32 @@ public class MiniGamesManager : MonoBehaviour
         LaunchRandomMinigame();
     }
 
-    private void LaunchRandomMinigame() //pour le moment c'est pas random mais belek
+    private void LaunchRandomMinigame()
     {
-        _jackCanvas.SetActive(true);
+        int r = Random.Range(0, _minigames.Count);
+        _currentMinigame =  _minigames[r];
+        _currentMinigame.SetActive(true);
+
+        if (_currentMinigame == _tapeTaupeCanvas)
+        {
+            _tapeTaupeManager.Play();
+        }
     }
 
     public void WinMiniGame()
     {
         _numberOfWins++;
-        _jackCanvas.SetActive(false);
+        _currentMinigame.SetActive(false);
         if (_numberOfWins < _amountOfWinsToEarn)
         {
             StartCoroutine(WaitBeforeLaunch());
+        }
+        else
+        {
+            _currentMinigame.SetActive(false);
+            _playerController.HideCursor();
+            _numberOfWins = 0;
+            _amountOfWinsToEarn = 3;
         }
     }
     
--- Assets/02_Scripts/MiniGames/TapeTaupe.cs
+++ Assets/02_Scripts/MiniGames/TapeTaupe.cs
@@ -1,30 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TapeTaupe : MonoBehaviour
 {
     [SerializeField] private Button _button1,  _button2, _button3, _button4, _button5, _button6, _button7, _button8, _button9;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private List<Button> _buttons, _activeButtons;
+    
+    private MiniGamesManager _manager;
+    
+    private void Awake()
     {
-        Inactivate(_button1);
-        Inactivate(_button2);
-        Inactivate(_button3);
-        Inactivate(_button4);
-        Inactivate(_button5);
-        Inactivate(_button6);
-        Inactivate(_button7);
-        Inactivate(_button8);
-        Inactivate(_button9);
+        _manager = FindFirstObjectByType<MiniGamesManager>();
+        
+        _buttons = new List<Button>();
+        _activeButtons = new List<Button>();
+        _buttons.Add(_button1);
+        _buttons.Add(_button2);
+        _buttons.Add(_button3);
+        _buttons.Add(_button4);
+        _buttons.Add(_button5);
+        _buttons.Add(_button6);
+        _buttons.Add(_button7);
+        _buttons.Add(_button8);
+        _buttons.Add(_button9);
+
+        foreach (var button in _buttons)
+        {
+            Inactivate(button);
+        }
     }
     
     private void Inactivate(Button button)
     {
         button.gameObject.GetComponent<Image>().color = new Color32(0, 0, 0, 255);
+        if (_activeButtons.Contains(button))
+        {
+            _activeButtons.Remove(button);
+        }
     }
 
     private void Activate(Button button)
     {
+        if (_activeButtons.Contains(button))
+        {
+            return;
+        }
         button.gameObject.GetComponent<Image>().color = new Color32(225, 205, 76, 255);
+        _activeButtons.Add(button);
+    }
+
+    public void Play()
+    {
+        int r =  Random.Range(0, _buttons.Count);
+        Activate(_buttons[r]);
+        StartCoroutine(CheckActiveButtonsToDisable());
+    }
+    
+    public void ClickOnButton(Button button)
+    {
+        if (_activeButtons.Contains(button))
+        {
+            Inactivate(button);
+            _manager.WinMiniGame();
+        }
+    }
+
+    private IEnumerator CheckActiveButtonsToDisable()
+    {
+        int r = Random.Range(1, 2);
+        yield return new WaitForSeconds(r);
+        if (_activeButtons.Count > 0)
+        {
+            foreach (var button in _activeButtons)
+            {
+                Inactivate(button);
+                break;
+            }
+        }
+        Play();
     }
 }
